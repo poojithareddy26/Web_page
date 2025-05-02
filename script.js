05d04b9f366d129045653bd3edfb08af95eef0af
m_poojithareddy <148128210+poojithareddy26@users.noreply.github.com>
2025-05-02 23:44:42 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+// script.js
+
+const animatedElements = document.querySelectorAll('.animate');
+
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.style.opacity = 1;
+            entry.target.style.transform = 'translateY(0)';
+        }
+    });
+}, {
+    threshold: 0.1
+});
+
+animatedElements.forEach(el => observer.observe(el));
